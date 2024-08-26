c917454174e66bacc83b7fb6ec78aa91a89917db
ggomchang <changhyunzzang01@gmail.com>
2024-08-27 00:07:34 +0900

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -225,3 +225,81 @@ resource "aws_security_group" "app_sg" {
     "karpenter.sh/discovery": "apdev-eks-cluster"
   }
 }
+
+resource "aws_security_group" "endpoint_sg" {
+  vpc_id = aws_vpc.vpc.id
+  name = "endpoint_sg"
+
+  ingress{
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = "0"
+    to_port = "0"
+  }
+
+  egress{
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = "0"
+    to_port = "0"
+  }
+
+  tags = {
+    Name = "endpoint_sg"
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr_endpoint" {
+  vpc_id = aws_vpc.vpc.id
+  service_name = "com.amazonaws.ap-northeast-2.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+
+  subnet_ids = [
+    aws_subnet.private_subnet_a.id,
+    aws_subnet.private_subnet_b.id
+  ]
+
+  security_group_ids = [ 
+    aws_security_group.endpoint_sg.id
+  ]
+
+  tags = {
+    Name = "ecr_dkr_endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_api_endpoint" {
+  vpc_id = aws_vpc.vpc.id
+  service_name = "com.amazonaws.ap-northeast-2.ecr.api"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+
+  subnet_ids = [
+    aws_subnet.private_subnet_a.id,
+    aws_subnet.private_subnet_b.id
+  ]
+
+  security_group_ids = [ 
+    aws_security_group.endpoint_sg.id
+  ]
+
+  tags = {
+    Name = "ecr_api_endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3_endpoint" {
+  vpc_id = aws_vpc.vpc.id
+  service_name = "com.amazonaws.ap-northeast-2.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [ 
+    aws_route_table.private_rt_a.id,
+    aws_route_table.private_rt_b.id
+  ]
+
+  tags = {
+    Name = "s3_endpoint"
+  }
+}
